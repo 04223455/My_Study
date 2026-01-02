ed3411acd54f03af30f515a9efbfd289f7390972
Xiaotong <1229010008@qq.com>
2026-01-02 22:28:04 +0800

    今天做了股价计算程序

---
+++ python_code/3.num_type.py
@@ -0,0 +1,12 @@
+# 整数转字符串类型
+str_num = str(11)
+print(type(str_num))
+print(str_num)
+
+# 整数转浮点数类型
+num_float = float(11) 
+print(type(num_float),num_float)
+
+# 浮点数转整数类型
+num1 = int(11.845)
+print(type(num1),num1)
---
+++ python_code/5.字符串扩展.py
@@ -0,0 +1,4 @@
+name = "黑马程序员"
+name1 = '黑马程序员'
+name2 = """黑马程序员"""
+print(name, name1, name2)
